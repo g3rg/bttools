c867e813b7000c77915f2fb936b703e7cf94814e
Greg <mauve.simian@gmail.com>
2024-01-25 12:19:01 +1000

    Front end running and routes working

---
+++ packages/frontend/src/containers/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Battletech Tools</h1>
+                <p className="text-muted">Its just like, tools for Battletech, man.</p>
+            </div>
+        </div>
+    );
+}
