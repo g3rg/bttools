4ac70582ec97de5dc5d6e589638e6e6e1238b258
Greg <mauve.simian@gmail.com>
2024-01-25 21:36:55 +1000

    Dodgy name and tonnage filtering working

--- packages/frontend/src/containers/UnitList.tsx
+++ packages/frontend/src/containers/UnitList.tsx
@@ -11,6 +11,9 @@ const dummyData = mechdata;
 export default function UnitList() {
     const { isAuthenticated } = useAppContext()
     const [isLoading, setIsLoading] = useState(true)
+    const [unitFilter, setUnitFilter] = useState("")
+    const [minTonFilter, setMinTonFilter] = useState("")
+    const [maxTonFilter, setMaxTonFilter] = useState("")
 
     useEffect(() => {
         async function onLoad() {
@@ -24,13 +27,63 @@ export default function UnitList() {
         onLoad()
     }, [isAuthenticated])
 
+    function doFilter(unit: UnitType) {
+        let show = true
+
+        if (unitFilter !== "" && !(unit.name.toLowerCase().indexOf(unitFilter.toLowerCase()) >= 0)) {
+            show = false
+        }
+
+        if (minTonFilter !== "") {
+            let minTons = parseInt(minTonFilter) // Todo Handle non-integers / force the field to be an int
+            if (parseInt(unit.tons || "100") < minTons) {
+                show = false
+            }
+        }
+
+        if (maxTonFilter !== "") {
+            let maxTons = parseInt(maxTonFilter) // Todo as above
+            if (parseInt(unit.tons || "0") > maxTons) {
+                show = false
+            }
+        }
+
+        return show
+    }
+
     function renderUnitList(units: UnitType[]) {
         return (
             <Table striped bordered hover size="sm" responsive="sm">
                 <thead>
                 <tr>
-                    <th>Unit</th>
-                    <th>Tons</th>
+                    <th>
+                        Unit<br/>
+                        <input
+                            type="text"
+                            value={unitFilter}
+                            onChange={(e) =>
+                                setUnitFilter(e.target.value)
+                            }
+                        />
+                    </th>
+                    <th>
+                        Tons<br/>
+                        <input
+                            type="text"
+                            value={minTonFilter}
+                            onChange={(e) =>
+                                setMinTonFilter(e.target.value)
+                            }
+                        />
+                        to
+                        <input
+                        type="text"
+                        value={maxTonFilter}
+                        onChange={(e) =>
+                            setMaxTonFilter(e.target.value)
+                        }
+                    />
+                    </th>
                     <th>BV</th>
                     <th>PV</th>
                     <th>Role</th>
@@ -40,18 +93,23 @@ export default function UnitList() {
                 </tr>
                 </thead>
                 <tbody>
-                { units.map( (unit) => (
-                    <tr>
-                        <td>{unit.name}</td>
-                        <td>{unit.tons}</td>
-                        <td>{unit.bv}</td>
-                        <td>{unit.pv}</td>
-                        <td>{unit.role}</td>
-                        <td>{unit.rules}</td>
-                        <td>{unit.intro}</td>
-                        <td>...</td>
-                    </tr>
-                ))}
+                { units.map( (unit) => {
+                    if (doFilter(unit)) {
+                        return (
+                            <tr>
+                                <td>{unit.name}</td>
+                                <td>{unit.tons}</td>
+                                <td>{unit.bv}</td>
+                                <td>{unit.pv}</td>
+                                <td>{unit.role}</td>
+                                <td>{unit.rules}</td>
+                                <td>{unit.intro}</td>
+                                <td>...</td>
+                            </tr>)
+                    } else {
+                        return (<></>)
+                    }
+                })}
                 </tbody>
             </Table>
         )
