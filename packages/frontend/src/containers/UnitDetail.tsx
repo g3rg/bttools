ef7e4933b5519526831c67185fb4c699cb3c84e4
Greg <mauve.simian@gmail.com>
2024-01-29 17:23:47 +1000

    Make Unit Detail look a little better

--- packages/frontend/src/containers/UnitDetail.tsx
+++ packages/frontend/src/containers/UnitDetail.tsx
@@ -1,4 +1,5 @@
 import {UnitType} from "../types/unit.ts";
+import {Table} from "react-bootstrap";
 
 interface UnitProps {
     unit?: UnitType
@@ -7,38 +8,56 @@ interface UnitProps {
 export default function UnitDetail( props : UnitProps) {
     // Not display Data Issues yet
     let unit = props.unit
+
+    function renderField(fieldName?: string, fieldValue?: string|number) {
+            if (fieldValue) {
+                    return (
+                        <><b>{fieldName}</b>: {fieldValue}<br/></>
+                    )
+            }
+            return (
+                <></>
+            )
+    }
+
     return (
         <>
-            Chassis: {unit?.chassis}<br/>
-            Variant: {unit?.variant}<br/>
-            Mass: {unit?.tons}<br/>
-            BV: {unit?.bv}<br/>
-            PV: {unit?.pv}<br/>
-            Role: {unit?.role}<br/>
-            Rules: {unit?.rules}<br/>
-            Introduction: {unit?.intro}<br/>
-            Config: {unit?.config}<br/>
-            Tech: {unit?.techbase}<br/>
-            Engine: {unit?.engine}<br/>
-            Internal Structure: {unit?.structure}<br/>
-            Myomer: {unit?.myomer}<br/>
-            Cockpit: {unit?.cockpit}<br/>
-            Gyro: {unit?.gyro}<br/>
-            Ejection: {unit?.ejection}<br/>
-            Heat Sinks: {unit?.heatSinks}<br/>
-            Walk MP: {unit?.walkMP}<br/>
-            Jump MP: {unit?.jumpMP}<br/>
-            Armor Type: {unit?.armorType}<br/>
-            Armor Points: {unit?.armorPoints}<br/>
-            Weapons:<br/> {
-                unit?.weapons?.map( (weapon) => {
+            {renderField('Chassis', unit?.chassis)}
+            {renderField('Variant', unit?.variant)}
+            {renderField('Mass', unit?.tons)}
+            {renderField('BV', unit?.bv)}
+            {renderField('PV', unit?.pv)}
+            {renderField('Role', unit?.role)}
+            {renderField('Rules', unit?.rules)}
+            {renderField('Introduction', unit?.intro)}
+            {renderField('Config', unit?.config)}
+            {renderField('Tech', unit?.techbase)}
+            {renderField('Engine', unit?.engine)}
+            {renderField('Internal Structure',unit?.structure)}
+            {renderField('Myomer', unit?.myomer)}
+            {renderField('Cockpit', unit?.cockpit)}
+            {renderField('Gyro', unit?.gyro)}
+            {renderField('Ejection', unit?.ejection)}
+            {renderField('Heat Sinks', unit?.heatSinks)}
+            {renderField('Walk MP', unit?.walkMP)}
+            {renderField('Jump MP', unit?.jumpMP)}
+            {renderField('Armor Type', unit?.armorType)}
+            {renderField('Armor Points', unit?.armorPoints)}
+            <b>Weapons:</b>
+                <Table id="weaponTable" striped bordered hover size="sm" responsive="sm">
+                {
+                    unit?.weapons?.map( (weapon) => {
                     return (
-                        <>
-                            {weapon.type}<br/>
-                        </>
-                    )
-                })
-            }
+
+                        <tr>
+                            <th>{weapon.type}</th>
+                            <th>{weapon.location}</th>
+                            <th>{weapon.ammo}</th>
+                        </tr>
+                        )
+                    })
+                }
+                </Table>
         </>
     )
 }
